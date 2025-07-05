333a36ca8fb3c19c69cc2a22b573ff28145daa0b
Gourav Sarkar <gouravsarkar2006@gmail.com>
2025-07-05 12:42:08 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,9 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyBqC0sd9K06Jp6cB-Xc2XOevcYIO2mCicI",
   authDomain: "activityzen-800b0.firebaseapp.com",
   projectId: "activityzen-800b0",
-  storageBucket: "activityzen-800b0.appspot.com",
+  storageBucket: "activityzen-800b0.firebasestorage.app",
   messagingSenderId: "36336996993",
-  appId: "1:36336996993:web:1675279f0a158b301ef380"
+  appId: "1:36336996993:web:1675279f0a158b301ef380",
+  measurementId: "G-E57RXPJ90W"
 };
 
 // Initialize Firebase
