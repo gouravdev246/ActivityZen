61b65575f7ac1f63a38dd1d072751b7166a63cc6
Gouravdev246 <gouravdev246@gmail.com>
2025-07-05 22:01:20 +0530

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { AuthProvider } from '@/context/auth-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Activity Tracker',
@@ -26,6 +27,7 @@ export default function RootLayout({
           {children}
         </AuthProvider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
