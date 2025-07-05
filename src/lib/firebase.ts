05f62fed475529270c82d8c5d3d2d06998287915
Gourav Sarkar <gouravsarkar2006@gmail.com>
2025-07-05 11:00:07 +0000

    login page not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBqC0sd9K06Jp6cB-Xc2XOevcYIO2mCicI",
+  authDomain: "activityzen-800b0.firebaseapp.com",
+  projectId: "activityzen-800b0",
+  storageBucket: "activityzen-800b0.appspot.com",
+  messagingSenderId: "36336996993",
+  appId: "1:36336996993:web:1675279f0a158b301ef380"
 };
 
 // Initialize Firebase
